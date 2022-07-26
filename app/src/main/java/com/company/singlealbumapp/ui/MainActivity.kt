4f431270404d5ecb95feabd2757d06bad015eb5c
ga1duk <gaidukdg@yandex.ru>
2022-07-26 18:30:00 +0300

    MediaApiService interface is added; Ability to make requests to the server is added; OkHttp, Retrofit are configured; Coroutines library is added

--- app/src/main/java/com/company/singlealbumapp/ui/MainActivity.kt
+++ app/src/main/java/com/company/singlealbumapp/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.company.singlealbumapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.company.singlealbumapp.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.company.singlealbumapp.api.MediaApi
+import com.company.singlealbumapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            try {
+                val body = MediaApi.retrofitService.getAlbum()
+                binding.tvTitle.text = body.title
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
