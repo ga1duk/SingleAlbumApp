d9ea53396c54d4629c1f2b8d782f086b4a81dc23
ga1duk <gaidukdg@yandex.ru>
2022-07-26 21:29:59 +0300

    recyclerView is added to app;

---
+++ app/src/main/java/com/company/singlealbumapp/adapter/TrackAdapter.kt
@@ -0,0 +1,52 @@
+package com.company.singlealbumapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.company.singlealbumapp.databinding.CardTrackBinding
+import com.company.singlealbumapp.dto.Track
+
+interface OnInteractionListener {
+    fun onClick(track: Track) {}
+}
+
+class TrackAdapter(private val listener: OnInteractionListener) :
+    ListAdapter<Track, TrackViewHolder>(TrackDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val binding = CardTrackBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrackViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        val track = getItem(position)
+        holder.bind(track)
+    }
+}
+
+
+class TrackViewHolder(
+    private val binding: CardTrackBinding,
+    private val listener: OnInteractionListener
+) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(track: Track) {
+        with(binding) {
+            tvTrackName.text = track.file
+            btnPlay.setOnClickListener {
+                listener.onClick(track)
+            }
+        }
+    }
+}
+
+class TrackDiffCallback : DiffUtil.ItemCallback<Track>() {
+    override fun areItemsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/company/singlealbumapp/ui/MainActivity.kt
+++ app/src/main/java/com/company/singlealbumapp/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.company.singlealbumapp.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.company.singlealbumapp.adapter.OnInteractionListener
+import com.company.singlealbumapp.adapter.TrackAdapter
 import com.company.singlealbumapp.api.MediaApiService
 import com.company.singlealbumapp.databinding.ActivityMainBinding
+import com.company.singlealbumapp.dto.Track
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,10 +24,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val adapter = TrackAdapter(object : OnInteractionListener {
+            override fun onClick(track: Track) {
+                Toast.makeText(this@MainActivity, track.file, Toast.LENGTH_LONG).show()
+            }
+        })
+
+        binding.rvTracks.adapter = adapter
+
+        val tracks = mutableListOf<Track>()
+
         lifecycleScope.launch {
             try {
                 val body = apiService.getAlbum()
-                binding.tvTitle.text = body.title
+                with(binding) {
+                    tvAlbumName.text = body.title
+                    tvAuthorName.text = body.artist
+                    tvAlbumYear.text = body.published
+                    tvAlbumGenre.text = body.genre
+                }
+                for (track in body.tracks) {
+                    tracks.add(Track(track.id, track.file))
+                }
+                adapter.submitList(tracks)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
