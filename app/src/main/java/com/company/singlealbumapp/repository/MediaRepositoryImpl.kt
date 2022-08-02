4cddd1f5e38b09c041b17f48904eca8d85cc2428
ga1duk <gaidukdg@yandex.ru>
2022-08-03 00:29:01 +0300

    Exception handling is added to getAlbum() function

--- app/src/main/java/com/company/singlealbumapp/repository/MediaRepositoryImpl.kt
+++ app/src/main/java/com/company/singlealbumapp/repository/MediaRepositoryImpl.kt
@@ -2,12 +2,15 @@ package com.company.singlealbumapp.repository
 
 import com.company.singlealbumapp.BuildConfig.BASE_URL
 import com.company.singlealbumapp.dto.Album
+import com.company.singlealbumapp.error.NetworkError
+import com.company.singlealbumapp.error.UnknownError
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.IOException
 import javax.inject.Inject
 
 class MediaRepositoryImpl @Inject constructor(private val client: OkHttpClient) : MediaRepository {
@@ -20,15 +23,21 @@ class MediaRepositoryImpl @Inject constructor(private val client: OkHttpClient) 
         lateinit var album: Album
         withContext(Dispatchers.IO) {
             runCatching {
-                val request: Request = Request.Builder()
-                    .url("$BASE_URL/album.json")
-                    .build()
-                album = client.newCall(request)
-                    .execute()
-                    .let { it.body?.string() ?: throw RuntimeException("body is null") }
-                    .let {
-                        gson.fromJson(it, typeToken.type)
-                    }
+                try {
+                    val request: Request = Request.Builder()
+                        .url("$BASE_URL/album.json")
+                        .build()
+                    album = client.newCall(request)
+                        .execute()
+                        .let { it.body?.string() ?: throw RuntimeException("body is null") }
+                        .let {
+                            gson.fromJson(it, typeToken.type)
+                        }
+                } catch (e: IOException) {
+                    throw NetworkError
+                } catch (e: Exception) {
+                    throw UnknownError
+                }
             }
         }
         return album
